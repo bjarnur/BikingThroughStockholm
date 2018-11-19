b20fdff716ff3107e2efdc8d6e054a378ba423ea
Sonia <s.camacho.her@gmail.com>
2018-11-19 17:35:27 +0100

    Detect FootLeft position

--- Assets/KinectView/Scripts/BodySourceView.cs
+++ Assets/KinectView/Scripts/BodySourceView.cs
@@ -3,12 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Kinect = Windows.Kinect;
 
+using Joint = Windows.Kinect.Joint;
+
 public class BodySourceView : MonoBehaviour 
 {
     public Material BoneMaterial;
     public GameObject BodySourceManager;
+
     
     private Dictionary<ulong, GameObject> _Bodies = new Dictionary<ulong, GameObject>();
+    private List<Kinect.JointType> _joints = new List<Kinect.JointType>
+    {
+        Kinect.JointType.FootLeft,
+        Kinect.JointType.FootRight,
+    };
     private BodySourceManager _BodyManager;
     
     private Dictionary<Kinect.JointType, Kinect.JointType> _BoneMap = new Dictionary<Kinect.JointType, Kinect.JointType>()
@@ -111,20 +119,38 @@ public class BodySourceView : MonoBehaviour
     {
         GameObject body = new GameObject("Body:" + id);
         
+        // Create joints
+        //foreach (Kinect.JointType joint in _joints)
+        //{
+        //    GameObject newJoint = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //    newJoint.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+        //    newJoint.name = joint.ToString();
+
+        //    newJoint.transform.parent = body.transform;
+        //}
         for (Kinect.JointType jt = Kinect.JointType.SpineBase; jt <= Kinect.JointType.ThumbRight; jt++)
         {
-            GameObject jointObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            
+            GameObject jointObj;
+            if (jt == Kinect.JointType.FootLeft || jt == Kinect.JointType.FootRight)
+            {
+                jointObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                jointObj.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            else
+            {
+                jointObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                jointObj.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            }
+
             LineRenderer lr = jointObj.AddComponent<LineRenderer>();
             lr.SetVertexCount(2);
             lr.material = BoneMaterial;
             lr.SetWidth(0.05f, 0.05f);
-            
-            jointObj.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+
             jointObj.name = jt.ToString();
             jointObj.transform.parent = body.transform;
         }
-        
+
         return body;
     }
     
@@ -142,6 +168,11 @@ public class BodySourceView : MonoBehaviour
             
             Transform jointObj = bodyObject.transform.Find(jt.ToString());
             jointObj.localPosition = GetVector3FromJoint(sourceJoint);
+            if(jt == Kinect.JointType.FootLeft)
+            {
+                print(jointObj.localPosition);
+                //-4.2 (Down) -2.1 (Up) -> set the threshold at 3
+            }
             
             LineRenderer lr = jointObj.GetComponent<LineRenderer>();
             if(targetJoint.HasValue)
