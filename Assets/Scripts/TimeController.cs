87e9f5f32fbb9f30ebf412d75a5a4a38c84934b6
Bjarni <bjarniragnarg@gmail.com>
2018-11-19 21:04:41 +0100

    Added basic timer

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour {
+
+    
+    private float timeSoFar;
+    private Text textField;
+
+	void Start ()
+    {
+        timeSoFar = 0.0f;
+        textField = GetComponent<Text>();
+	}
+	
+	
+	void Update ()
+    {
+        timeSoFar += Time.deltaTime;
+        string minutes = Mathf.Floor(timeSoFar / 60).ToString("00");
+        string seconds = (timeSoFar % 60).ToString("00");       
+        textField.text = minutes + ":" + seconds;
+    }
+}
