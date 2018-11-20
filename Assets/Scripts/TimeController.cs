0e5aa04c94a81bc3b46e6a0c2897caaf97566449
Bjarni <bjarniragnarg@gmail.com>
2018-11-20 21:22:47 +0100

    Milestones at regular intervals with random compliment phrases

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -5,22 +5,87 @@ using UnityEngine.UI;
 
 public class TimeController : MonoBehaviour {
 
-    
+    [SerializeField] private float rewardInterval;
+    [SerializeField] GameObject milestonePanel;
+    [SerializeField] GameObject timePrompt;
+
+    private GameObject milestonePrompt;
+    private GameObject complimentPrompt;
+
     private float timeSoFar;
-    private Text textField;
+    private float lastMilestone;
+    private string secondsSoFar;
+    private string minutesSoFar;
+
+    private Text timeText;
+    private Text milestoneText;
+    private Text complimentText;
+
+    private static List<string> compliments = new List<string>
+    {
+        "Go you!!", 
+        "Well done!!",
+        "Yowza!",
+        "Biking saves money and burns calories",
+        "I like crayons"
+    };
 
 	void Start ()
     {
         timeSoFar = 0.0f;
-        textField = GetComponent<Text>();
+
+        milestonePrompt = milestonePanel.transform.GetChild(0).gameObject;
+        complimentPrompt = milestonePanel.transform.GetChild(1).gameObject;
+
+        timeText = timePrompt.GetComponent<Text>();
+        milestoneText = milestonePrompt.GetComponent<Text>();
+        complimentText = complimentPrompt.GetComponent<Text>();
 	}
 	
 	
 	void Update ()
     {
+        UpdateTimer();
+        CheckMilestones();
+    }
+
+    private void UpdateTimer()
+    {
         timeSoFar += Time.deltaTime;
-        string minutes = Mathf.Floor(timeSoFar / 60).ToString("00");
-        string seconds = (timeSoFar % 60).ToString("00");       
-        textField.text = minutes + ":" + seconds;
+        minutesSoFar = Mathf.Floor(timeSoFar / 60).ToString("00");
+        secondsSoFar = (timeSoFar % 60).ToString("00");
+        timeText.text = minutesSoFar + ":" + secondsSoFar;
+    }
+
+    private void CheckMilestones()
+    {
+        float progressSinceLastMilestone = timeSoFar - lastMilestone;
+        if(progressSinceLastMilestone > rewardInterval)
+        {
+            Debug.Log("progress since last " + progressSinceLastMilestone);
+            Debug.Log("time so far " + timeSoFar);
+            Debug.Log("reward intervarl " + rewardInterval);
+            StartCoroutine("DisplayReward", 2);
+            lastMilestone = timeSoFar;
+        }
+    }
+
+    private IEnumerator DisplayReward(float seconds)
+    {
+        float timeActive = 0.0f;                
+
+        int complimentIndex = Random.Range(0, compliments.Count);
+        milestoneText.text = minutesSoFar + ":" + secondsSoFar;
+        complimentText.text = compliments[complimentIndex];
+
+        milestonePanel.SetActive(true);
+        while (timeActive < seconds)
+        {
+            timeActive += Time.deltaTime;
+            yield return null;
+        }
+
+        milestonePanel.SetActive(false);
+        yield break;
     }
 }
