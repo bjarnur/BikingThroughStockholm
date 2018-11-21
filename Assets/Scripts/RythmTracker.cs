df93605d7640e2e1d932e40e5df1735288c93bc7
VIC <>
2018-11-21 12:04:43 +0100

    Adds rythm tracker

---
+++ Assets/Scripts/RythmTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RythmTracker : MonoBehaviour
+{
+
+    public Image ProgressBar;
+    private float progress;
+
+	// Use this for initialization
+	void Start ()
+    {
+        progress = 0.6f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        progress = Mathf.Max(progress - 0.1f * Time.deltaTime, 0f);
+        if (Input.GetKeyDown("space"))
+        {
+            progress = Mathf.Min(progress + 0.1f, 1.0f);
+        }
+
+        ProgressBar.fillAmount = progress;
+	}
+}
